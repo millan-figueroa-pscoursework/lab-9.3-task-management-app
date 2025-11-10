8a7b2fa182f4850f9aa06bcfb93d82f288cf9be9
millan-figueroa <millan.fig@gmail.com>
2025-11-09 21:38:46 -0800

    Adds task description attribute to Tasklist, implements component in App.tsx. Hard codes first task with dummy data, implements TaskList component in parent with placeholder functions

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -1,16 +1,18 @@
 import type { TaskListProps } from "../../types";
 
 export default function TaskList({ tasks }: TaskListProps) {
-  // if there's not tasks renders a div
   if (tasks.length === 0) {
     return <div>No tasks yet.</div>;
   }
 
   return (
-    // lops thru tasks array and renders li element with unique key
     <ul>
       {tasks.map((task) => (
-        <li key={task.id}>{task.title}</li>
+        <li key={task.id} style={{ marginBottom: "1rem" }}>
+          <h3>{task.title}</h3>
+          <p>{task.description}</p>
+          <p>Status: {task.status}</p>
+        </li>
       ))}
     </ul>
   );
